87882e33f92f73ff294f32b4b1bac697cd76563d
PercivalN <percival.ngan@gmail.com>
2020-05-27 16:30:55 -0500

    Finished alternating character

--- AlternatingCharacters.playground/Contents.swift
+++ AlternatingCharacters.playground/Contents.swift
@@ -2,9 +2,21 @@ import UIKit
 
 func alternatingCharacters(s: String) -> Int {
     
-    if c.characters.count < 2 { return 0 }
+    if s.count < 2 { return 0 }
     
-    var characterArray = Array(s.characters)
-    var current = chars[0]
+    let characterArray = Array(s)
+    var currentCharacter = characterArray[0]
+    var counter = 0
     
+    for i in (1 ..< characterArray.count) {
+        if currentCharacter == characterArray[i] {
+            counter += 1
+        } else {
+            currentCharacter = characterArray[i]
+        }
+    }
+    return counter
 }
+
+
+alternatingCharacters(s: "dferrds")
