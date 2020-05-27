8394dc92664247a18ed282e60e8cf97f33c6bd57
PercivalN <percival.ngan@gmail.com>
2020-05-26 21:31:05 -0500

    Initial commit

---
+++ AlternatingCharacters.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+func alternatingCharacters(s: String) -> Int {
+    
+    if c.characters.count < 2 { return 0 }
+    
+    var characterArray = Array(s.characters)
+    var current = chars[0]
+    
+}
